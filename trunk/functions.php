b789a56db9becd64f1655d9e1ac2b60709065e8b
pendergast.gary <pendergast.gary@2179d40f-1b76-1eaf-c469-39d685390f91>
2010-05-25 22:31:17 +0000

    - Steam connector - Simple shortcode

--- trunk/functions.php
+++ trunk/functions.php
@@ -1,5 +1,47 @@
 <?php
 
+function steam_stats_get_data( $username ) {
+	$options = get_option( 'steam_stats_options' );
+	
+	// Check if the cache doesn't exist, or if it's older than 15 minutes.
+	if( ! array_key_exists( $username, $options['cache'] ) || $options['cache'][$username]['timestamp'] + 15*60 < time() ) {
+		$profile = simplexml_load_file( "http://steamcommunity.com/id/$username/?xml=1", 'SimpleXMLElement', LIBXML_NOCDATA );
+		$games = simplexml_load_file( "http://steamcommunity.com/id/$username/games/?xml=1" );
+		
+		if( ! $profile )
+			return new WP_Error( 'steam_profile_retrieval_failed', __( 'The request to Steam Community failed.', 'steam-stats' ) );
+
+		if( isset( $profile->error ) )
+			return new WP_Error( 'steam_server_error', sprintf( __( 'Steam returned the following error: %1s', 'steam-stats' ), $profile->error ) );
+			
+		$options['cache'][$username]['profile'] = $profile->asXML();
+		$options['cache'][$username]['games'] = $games->asXML();
+		$options['cache'][$username]['timestamp'] = time();
+		
+		update_option( 'steam_stats_options', $options );
+	}
+	
+	$data = $options['cache'][$username];
+	
+	$data['profile'] = simplexml_load_string( $data['profile'], 'SimpleXMLElement', LIBXML_NOCDATA );
+	$data['games'] = simplexml_load_string( $data['games'], 'SimpleXMLElement', LIBXML_NOCDATA );
+	
+	return $data;
+}
+
+function steam_stats_display_init() {
+	wp_enqueue_style( 'steam-stats-display', STEAM_STATS_URL . '/css/display.css', false, STEAM_STATS_VERSION );
+}
+
+function steam_stats_plugin_row_meta( $links, $file ) {
+	if( STEAM_STATS_BASENAME == $file ) {
+		$links[] = '<a href="http://www.amazon.com/wishlist/1ORKI9ZG875BL">' . __( 'My Amazon Wish List', 'jobman' ) . '</a>';
+		$links[] = '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=gary%40pento%2enet&item_name=WordPress%20Plugin%20(Steam%20Stats)&item_number=Support%20Open%20Source&no_shipping=0&no_note=1&tax=0&currency_code=USD&lc=US&bn=PP%2dDonationsBF&charset=UTF%2d8">' . __( 'Donate with PayPal', 'jobman' ) . '</a>';
+	}
+	
+	return $links;
+}
+
 function steam_stats_load_translation_file() {
 	load_plugin_textdomain( 'steam_stats', '', STEAM_STATS_FOLDER . '/translations' );
 }
--- trunk/hooks.php
+++ trunk/hooks.php
@@ -13,6 +13,12 @@ foreach( $steam_stats_shortcodes as $sss ) {
 	add_shortcode( $sss, 'steam_stats_shortcode' );
 }
 
+// Add our init stuff
+add_action( 'init', 'steam_stats_display_init' );
+
+// Plugin settings links
+add_filter( 'plugin_row_meta', 'steam_stats_plugin_row_meta', 10, 2 );
+
 // Uninstall function
 if( function_exists( 'register_uninstall_hook' ) )
 	register_uninstall_hook( WP_PLUGIN_DIR . '/' . STEAM_STATS_FOLDER . '/steam-stats.php', 'steam_stats_uninstall' );
